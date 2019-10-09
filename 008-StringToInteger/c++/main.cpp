1f31d588111f6188d4cecee833261488c7eeea21
smbzhang <17862988206@163.com>
2019-10-09 14:45:41 +0100

    修改008 整数溢出判断

--- 008-StringToInteger/c++/main.cpp
+++ 008-StringToInteger/c++/main.cpp
@@ -11,7 +11,7 @@ class Solution {
 public:
     int myAtoi(string str) {
         int n = str.size(), i = 0;
-        long int res = 0;
+        int res = 0;
         bool flag = true;
         while(i < n && str[i] == ' ') i++;
         if (str[i] == '-' || str[i] == '+') {flag = (str[i] == '+') ? true : false; i++;}
@@ -19,11 +19,30 @@ public:
             if (str[i] > '9' || str[i] < '0') break;
             int num = str[i] - '0';
             if (flag && ((res > INT_MAX / 10) || (res == INT_MAX / 10 && num > INT_MAX % 10))) return INT_MAX;
-            if (!flag && ((-res < INT_MIN / 10) || (-res == INT_MIN / 10 && -num < INT_MIN % 10))) return INT_MIN;
+            if (!flag && ((-res < INT_MIN / 10) || (-res == INT_MIN / 10 && -num <= INT_MIN % 10))) return INT_MIN;
             res = res * 10 + num;  // 如果 flag = false， 负值的最大值在这里会溢出，所以使用 long long int 来存储 res
         }
         return (flag) ? res : -res;
     }
+
+    int myAtoi_2(string str) {
+        int n = str.size(), i = 0;
+        long long int res = 0;
+        bool flag = true;
+        while(i < n && str[i] == ' ') i++;
+        if (str[i] == '-' || str[i] == '+') {flag = (str[i] == '+') ? true : false; i++;}
+        for(; i < n; i++) {
+            if (str[i] > '9' || str[i] < '0') break;
+            int num = str[i] - '0';
+//            if (flag && ((res > INT_MAX / 10) || (res == INT_MAX / 10 && num > INT_MAX % 10))) return INT_MAX;
+//            if (!flag && ((-res < INT_MIN / 10) || (-res == INT_MIN / 10 && -num < INT_MIN % 10))) return INT_MIN;
+            res = res * 10 + num;  // 如果 flag = false， 负值的最大值在这里会溢出，所以使用 long long int 来存储 res
+            if (flag && res > INT_MAX) return INT_MAX;
+            if (!flag && res - 1 > INT_MAX) return INT_MIN;
+        }
+        return (flag) ? res : -res;
+    }
+
 };
 
 int main(int argc, char *argv[]) {
