2ebfa0b428ffe0f363bb1d19fa83f6351fc83488
smbzhang <17862988206@163.com>
2019-11-01 03:01:13 +0000

    041-FirstMissingPositive

---
+++ 041-FirstMissingPositive/c++/main.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include "common/cpp_common.h"
+#include <algorithm>
+#include <map>
+using namespace std;
+
+// https://leetcode.com/problems/first-missing-positive/
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            while(i < n && nums[i] != i + 1 && nums[i] < n && nums[i] > 0 && nums[i] != nums[nums[i] - 1]) {
+                std::swap(nums[i], nums[nums[i] - 1]);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1)
+                return i + 1;
+        }
+        return n + 1;
+    }
+};
+int main() {
+    vector<int> nums1; 
+    vector<int> nums2;
+    std::vector<std::string> strs;
+    std::string str;
+    leetcode::common::get_string_from_file("./data1.txt", str);
+    strs = leetcode::common::split(str,'\n');
+    Solution *solution = new Solution(); 
+    for (auto str : strs) {
+        cout << str << endl;
+        std::vector<string> array_s  = leetcode::common::split(str,',');
+        std::vector<int> nums;
+        leetcode::common::strings_to_numbers<int>(array_s, nums);
+        int result = solution->firstMissingPositive(nums);
+        cout << "result: " << result << endl;
+    }
+   
+    return 0;
+}
