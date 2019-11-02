e961eb96912cba73b8e23cd0ab2e2401eacb04df
smbzhang <17862988206@163.com>
2019-11-02 11:37:54 +0800

    041-FirstMissingPositive 添加第二种解法

--- 041-FirstMissingPositive/c++/main.cpp
+++ 041-FirstMissingPositive/c++/main.cpp
@@ -8,8 +8,13 @@
 using namespace std;
 
 // https://leetcode.com/problems/first-missing-positive/
+/**
+ * 这种题其实一开始想的是 bitmap 来做，但是并不能使用 bitmap 来做
+ * 其实本质就是使用一个 map 来做映射，然后进行查找
+ */
 class Solution {
 public:
+    // 解法一：利用数组的下标作为值,数组的值作为键
     int firstMissingPositive(vector<int>& nums) {
         int n = nums.size();
         for (int i = 0; i < n; i++) {
@@ -20,6 +25,30 @@ public:
         for (int i = 0; i < n; i++) {
             if (nums[i] != i + 1)
                 return i + 1;
+        }
+        return n + 1;
+    }
+    // 解法二：使用数组的下标作为key，数组值的符号作为value
+    int firstMissingPositive_2(vector<int> &nums) {
+        int n = nums.size();
+        bool hasone = false;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 1) {
+                hasone = true;
+                break;
+            } 
+        }
+        if (!hasone) return 1;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < 1 || nums[i] > n)
+                nums[i] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            int pos = std::abs(nums[i]);
+            nums[pos - 1] = nums[pos - 1] < 0 ? nums[pos - 1] : -nums[pos - 1];
+        }
+        for (int i= 0; i < n; i++) {
+            if (nums[i] > 0) return i + 1;
         }
         return n + 1;
     }
@@ -37,7 +66,7 @@ int main() {
         std::vector<string> array_s  = leetcode::common::split(str,',');
         std::vector<int> nums;
         leetcode::common::strings_to_numbers<int>(array_s, nums);
-        int result = solution->firstMissingPositive(nums);
+        int result = solution->firstMissingPositive_2(nums);
         cout << "result: " << result << endl;
     }
    
