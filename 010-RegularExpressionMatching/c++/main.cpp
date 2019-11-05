8b329f6a25f669a05e543669a227162508d37b07
smbzhang <17862988206@163.com>
2019-11-05 22:07:39 +0800

    010-RegularExpressionMatching 递归实现

--- 010-RegularExpressionMatching/c++/main.cpp
+++ 010-RegularExpressionMatching/c++/main.cpp
@@ -9,11 +9,21 @@ using namespace std;
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        return false;
+        if (p.empty()) return s.empty();
+        bool first_match = !s.empty() && (p[0] == s[0] || p[0] == '.');
+        if (p.length() > 1 && p[1] == '*') {
+            return (isMatch(s, p.substr(2)) || \
+                    (first_match && isMatch(s.substr(1), p)));
+        }else{
+            return first_match && isMatch(s.substr(1), p.substr(1));
+        }
     }
 };
 
 int main(int argc, char *argv[]) {
+
+    Solution solution;
+    cout << solution.isMatch("aaaaa", ".*aaaa") << endl;
     
     return 0;
 }
