59ff75048b1ad8c143cc5d9ed16f222a3402c0ed
smbzhang <17862988206@163.com>
2019-11-06 21:31:31 +0800

    044-WildcardMatching

---
+++ 044-WildcardMatching/c++/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include "common/cpp_common.h"
+#include <unordered_map>
+#include <string>
+
+using namespace std;
+// https://leetcode.com/problems/regular-expression-matching/
+class Solution {
+public:
+    //解法一: 递归求解
+    bool isMatch(string s, string p) {
+        if (p.empty()) return s.empty();
+        bool first_match = !s.empty() && (p[0] == s[0] || p[0] == '.');
+        if (p.length() > 1 && p[1] == '*') {
+            return (isMatch(s, p.substr(2)) || \
+                    (first_match && isMatch(s.substr(1), p)));
+        }else{
+            return first_match && isMatch(s.substr(1), p.substr(1));
+        }
+    }
+
+    // 解法二：动态规划求解
+    bool isMatch_2(string s, string p) {
+        int n1 = s.length(), n2 = p.length();
+        vector<vector<bool> >dp(n1 + 1, vector<bool>(n2 + 1, true));
+        for (int i = n1; i >= 0; i--) {
+            for (int j = n2; j >= 0; j--) {
+                if (i == n1 && j == n2) continue;
+                if (j < n2 && p[j] == '*') {
+                    dp[i][j] = dp[i][j + 1] || (i < n1 && dp[i + 1][j]);
+                }
+                else {
+                    dp[i][j] = (i < n1 && j < n2) && (s[i] == p[j] || p[j] == '?') && dp[i + 1][j + 1];
+                }
+            }
+        }
+        return dp[0][0];
+    }
+};
+
+int main(int argc, char *argv[]) {
+
+    Solution solution;
+    cout << solution.isMatch_2("aa", "*") << endl;
+    
+    return 0;
+}
