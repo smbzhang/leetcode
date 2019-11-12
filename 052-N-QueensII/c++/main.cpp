8f9c4bf9dfe9a16b80703d4d1e9b7187233968fd
smbzhang <17862988206@163.com>
2019-11-12 13:36:12 +0800

    052-N-QueensII 优化了个寂寞

--- 052-N-QueensII/c++/main.cpp
+++ 052-N-QueensII/c++/main.cpp
@@ -63,13 +63,55 @@ public:
         }
         return true;
     }
+
+    int totalNQueens_2(int n) {
+        int result = 0;
+        vector<vector<char> > records(n, vector<char>(n, '.'));
+        backtrace_2(n, 0, records, result);
+        return result;
+    }
+
+    void backtrace_2(int n, int row, vector<vector<char> >& records, int &result) {
+        if (row == n) {
+            result++;
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            records[row][i] = 'Q';
+            if (isvalid_2(records, row, n)) backtrace_2(n, row + 1, records, result);
+            records[row][i] = '.';
+        }
+    }
+
+    bool isvalid_2(const vector<vector<char> >& records, int row, int n) {
+        vector<bool> tmp45(n + 2 * (n - 1), false);
+        vector<bool> tmp135(n + 2 * (n - 1), false);
+        vector<bool> tmpj(n, false);
+        for (int i = 0; i <= row; i++) {
+            for (int j = 0; j < n; j++) {
+                if (records[i][j] == 'Q') {
+                    if (tmpj[j] || tmp45[j + (n - 1 - i) + n - 1] || tmp135[j - (n - 1 - i) + n - 1]) return false;
+                    else {
+                        tmpj[j] = true; tmp45[j + (n - 1 - i) + n - 1] = true; tmp135[j - (n - 1 - i) + n - 1] = true;
+                    }
+                }
+            }
+        }
+        return true;
+    }
 };
 
 int main(int argc, char *argv[]) {
     int n = 0;
+    vector<vector<char> > records(4, vector<char>(4, '.'));
+    records[0][1] = 'Q';
+    records[1][3] = 'Q';
+    records[2][0] = 'Q';
+    records[3][2] = 'Q';
     Solution *solution = new Solution();
+    cout << solution->isvalid_2(records, 3, 4);
     while (cin >> n) {
-        int res = solution->totalNQueens(n);
+        int res = solution->totalNQueens_2(n);
         cout << res << endl;
     }
     
