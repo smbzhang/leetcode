f146d8003988eab6ca329cbfc81e3d4231205e59
smbzhang <17862988206@163.com>
2019-11-13 09:44:42 +0800

    053-MaximumSubarray 动态规划实现,待实现分治法求解

---
+++ 053-MaximumSubarray/c++/main.cpp
@@ -0,0 +1,56 @@
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include "common/cpp_common.h"
+#include <algorithm>
+#include <map>
+#include <climits>
+#include <unordered_map>
+using namespace std;
+
+// https://leetcode.com/problems/maximum-subarray/
+class Solution {
+public:
+    // 解法一： 动态规划实现
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size(), max = INT_MIN;
+        vector<int> records(n, 0);
+        for (int i = n - 1; i >= 0; i--) {
+            if (i == n - 1) {
+                records[i] = nums[i];
+                max = std::max(max, records[i]);
+            }            
+            else {
+                if (records[i + 1] > 0) {
+                    records[i] = nums[i] + records[i + 1];
+                    max = std::max(max, records[i]);
+                }else{
+                    records[i] = nums[i];
+                    max = std::max(max, records[i]);
+                }
+            }
+        }
+        return max;
+    }
+};
+
+int main() {
+    vector<int> nums1; 
+    vector<int> nums2;
+    std::vector<std::string> strs;
+    std::string str;
+    leetcode::common::get_string_from_file("./data1.txt", str);
+    strs = leetcode::common::split(str,'\n');
+    Solution *solution = new Solution(); 
+    for (auto str : strs) {
+        cout << str << endl;
+        std::vector<string> array_s  = leetcode::common::split(str,',');
+        std::vector<int> nums;
+        leetcode::common::strings_to_numbers<int>(array_s, nums);
+        int result = solution->maxSubArray(nums);
+        cout << result << endl;   
+    }
+
+    return 0;
+}
