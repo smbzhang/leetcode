6b9bc64589bce9bbdc0e5329f224bc7e0a9a0f96
smbzhang <17862988206@163.com>
2019-11-19 21:55:12 +0800

    062-UniquePaths 排列组合公式求解

--- 062-UniquePaths/c++/main.cpp
+++ 062-UniquePaths/c++/main.cpp
@@ -2,9 +2,14 @@
 #include "datastruct/data_struct.h"
 #include "common/cpp_common.h"
 #include <stack>
+#include <algorithm>
 
 using namespace std;
 // https://leetcode.com/problems/unique-paths/
+int factorial(int m) {
+    if (m <= 1) return 1;
+    return m * factorial(m - 1);
+}
 class Solution {
 public:
     // 解法一：回溯法，超时
@@ -35,13 +40,31 @@ public:
         }
         return dp[m - 1][n - 1];
     }
+
+    // 解法三：因为向下的步数是一定的，向右的步数也是一定的，总步数是 m + n - 2, 向下的步数是： n - 1。 所以结果是：Combination(n - 1, m + n - 2)
+    // Combination(k, n) = n!/(k!(n - k)!)
+    int uniquePaths_3(int m, int n) {
+        // 这样不可以， 首先 factorial 会溢出。还要相乘，更要溢出了
+        return factorial(m + n - 2) / (factorial(n - 1) * factorial(m - 1));
+    }
+
+    // 分析上面的排列式子，做相消处理
+    int uniquePaths_4(int m, int n) {
+        long long int result = 1;
+        int N = m + n - 2;
+        int K = m - 1;
+        for (int i = 1; i <= K; i++) {
+            result = result * (N - K + i) / i;
+        }
+        return static_cast<int>(result);
+    }
 };
 
 int main(int argc, char *argv[]) {
     Solution *solution = new Solution();
     int m, n;
     cin >> m >> n;
-    cout << solution->uniquePaths_2(m, n) << endl;
+    cout << solution->uniquePaths_4(m, n) << endl;
 
     return 0;
 }
