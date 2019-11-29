c3f730dd7e0c67b5999f6f5f87876ff9e1df3f91
smbzhang <17862988206@163.com>
2019-11-29 20:39:20 +0800

    100-SameTree 递归实现

---
+++ 100-SameTree/c++/main.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include "common/cpp_common.h"
+#include "datastruct/data_struct.h"
+#include <algorithm>
+#include <map>
+#include <stack>
+using namespace std;
+
+// https://leetcode.com/problems/same-tree/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == nullptr && q == nullptr) return true;
+        if (p == nullptr || q == nullptr) return false;
+        if (p->val != q->val) return false;
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
+
+int main() {
+    return 0;
+}
