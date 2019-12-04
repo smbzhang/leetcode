588a97d70aa2a3552b0516affa850758876047ab
smbzhang <17862988206@163.com>
2019-12-04 09:44:53 +0800

    104-MaximumDepthofBinaryTree 递归实现

---
+++ 104-MaximumDepthofBinaryTree/c++/main.cpp
@@ -0,0 +1,44 @@
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include "common/cpp_common.h"
+#include "datastruct/data_struct.h"
+#include <algorithm>
+#include <map>
+#include <stack>
+using namespace std;
+using namespace leetcode::common;
+
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    // 递归实现
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr) return 0;
+        return std::max(maxDepth(root->left) + 1, maxDepth(root->right) + 1);
+    }
+};
+
+int main() {
+    std::vector<std::string> strs;
+    std::string str;
+    leetcode::common::get_string_from_file("./data1.txt", str);
+    strs = leetcode::common::split(str,',');
+    TreeNode *root = leetcode::common::ConstructTree(strs);
+    
+    Solution *solution = new Solution();
+    cout << solution->maxDepth(root) << endl;
+
+    return 0;
+}
