e795f3993195ebb7dd22d007f161581cbfed0b5b
smbzhang <17862988206@163.com>
2019-12-11 02:27:24 +0000

    110-BalancedBinaryTree 自顶向下 DP 实现

--- 110-BalancedBinaryTree/c++/main.cpp
+++ 110-BalancedBinaryTree/c++/main.cpp
@@ -23,18 +23,42 @@ using namespace leetcode::common;
  */
 class Solution {
 public:
+    // 解法一： 递归
     bool isBalanced(TreeNode* root) {
         if (root == nullptr) return true;
         // 判断当前结点的左右子树是不是平衡
-        int lh = height(root->left), rh = height(root->right);
+        int lh = height_2(root->left), rh = height_2(root->right);
         // 判断当前结点的左右子树是不是都平衡
         return isBalanced(root->left) && isBalanced(root->right) && (std::abs(lh - rh) <= 1);
     }
 
+    int height_2(TreeNode *root) {
+        if (root == nullptr) return 0;
+        return std::max(height_2(root->left) + 1, height_2(root->right) + 1);
+    }
+
+    // 解法二：dp 自顶向下
+    bool isBalanced_2(TreeNode* root) {
+        if (root == nullptr) return true;
+        if (bmap.find(root) != bmap.end()) return bmap[root];
+        // 判断当前结点的左右子树是不是平衡
+        int lh = height(root->left), rh = height(root->right);
+        // 判断当前结点的左右子树是不是都平衡
+        bool ret = isBalanced(root->left) && isBalanced(root->right) && (std::abs(lh - rh) <= 1);
+        bmap[root] = ret;
+        return ret;
+    }
+
     int height(TreeNode *root) {
         if (root == nullptr) return 0;
-        return std::max(height(root->left) + 1, height(root->right) + 1);
+        if (hmap.find(root) != hmap.end()) return hmap[root];
+        int ret = std::max(height(root->left) + 1, height(root->right) + 1);
+        hmap[root] = ret;
+        return ret;
     }
+private:
+    std::map<TreeNode *, bool> bmap;
+    std::map<TreeNode *, bool> hmap;
 };
 
 int main() {
@@ -45,7 +69,7 @@ int main() {
     TreeNode *root = leetcode::common::ConstructTree(strs);
     
     Solution *solution = new Solution();
-    cout << solution->isBalanced(root) << endl;
+    cout << solution->isBalanced_2(root) << endl;
 
     return 0;
 }
