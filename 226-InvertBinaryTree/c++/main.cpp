b2b99c493f9889f9109a2620427cfe2e11ec409d
smbzhang <17862988206@163.com>
2019-12-25 13:18:02 +0000

    226-InvertBinaryTree

---
+++ 226-InvertBinaryTree/c++/main.cpp
@@ -0,0 +1,47 @@
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include "common/cpp_common.h"
+#include "datastruct/data_struct.h"
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <queue>
+using namespace std;
+using namespace leetcode::common;
+
+// https://leetcode.com/problems/invert-binary-tree/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr) return root;
+        auto left = invertTree(root->left);
+        auto right = invertTree(root->right);
+        root->left = right;
+        root->right = left;
+        return root;
+    }
+};
+
+int main() {
+    std::vector<std::string> strs;
+    std::string str;
+    leetcode::common::get_string_from_file("./data1.txt", str);
+    strs = leetcode::common::split(str,',');
+    TreeNode *root = leetcode::common::ConstructTree(strs);
+    
+    Solution *solution = new Solution();
+    solution->invertTree(root);
+
+    return 0;
+}
