9df46e7661e34e0fea56c9cfc75bb49285e37860
smbzhang <17862988206@163.com>
2019-12-28 17:59:11 +0800

    236-LowestCommonAncestorOfABinaryTree

---
+++ 236-LowestCommonAncestorOfABinaryTree/c++/main.cpp
@@ -0,0 +1,44 @@
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include "common/cpp_common.h"
+#include "datastruct/data_struct.h"
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <queue>
+using namespace std;
+using namespace leetcode::common;
+
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == nullptr || root == p || root == q){return root; }
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        return left == nullptr? right : (right == nullptr? left : root); 
+    }
+};
+
+int main() {
+    std::vector<std::string> strs;
+    std::string str;
+    leetcode::common::get_string_from_file("./data1.txt", str);
+    strs = leetcode::common::split(str,',');
+    TreeNode *root = leetcode::common::ConstructTree(strs);
+    
+    Solution *solution = new Solution();
+    cout << solution->lowestCommonAncestor(root, root->left, root->right)->val << endl;;
+    return 0;
+}
